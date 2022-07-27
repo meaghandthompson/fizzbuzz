bdbcfaa8e76139d69ee0db91e3a2b3d57af18734
Meaghan Thompson <meaghster@gmail.com>
2022-07-27 15:03:17 -0700

    parameterized fizzbuzz

--- src/index.js
+++ src/index.js
@@ -1,33 +1,7 @@
-const result = fizzBuzz2(1, 100);
+const result = fizzBuzz(1, 100, 10, 10, [{ value: 3, result: 'bl' }, { value: 5, result: 'ar' }, { value: 10, result: 'zz' }]);
 console.log(result);
 
 
-/**
- * Writes out the FizzBuzz for a range of numbers.
- * 
- * FizzBuzz is defined by the following rules:
- *  - if a number is divisible by 5, print fizz
- *  - if a number is divisible by 8, print buzz
- *  - if divisible by both 5 and 8, print fizzbuzz
- *  - otherwise just print the number
- * 
- * @param {*} min the lower end of the range
- * @param {*} max the upper end of the range
- */
-function fizzBuzz(min, max) {
-    for (let i = min; i <= max; i++) {
-        if (i % 5 === 0 && i % 8 === 0) {
-            console.log('fizzbuzz');
-        } else if (i % 8 === 0) {
-            console.log('buzz');
-        } else if (i % 5 === 0) {
-            console.log('fizz');
-        } else {
-            console.log(i);
-        }
-        
-    }
-}
 
 /**
  * Writes out the FizzBuzz for a range of numbers.
@@ -40,32 +14,40 @@ function fizzBuzz(min, max) {
  * 
  * @param {*} min the lower end of the range
  * @param {*} max the upper end of the range
+ * @param {*} padding the amount of padding
+ * @param {*} entriesPerLine the frequency of new lines
  */
- function fizzBuzz2(min, max) {
+function fizzBuzz(
+    min, 
+    max, 
+    padding = 10, 
+    entriesPerLine = 10, 
+    tests = [{ value: 5, result: 'fizz' }, { value: 8, result: 'buzz' }]
+) {
     let result = "";
     for (let i = min; i <= max; i++) {
         let entry = "";
-        entry = fizzBuzzAt(i);
-        result = result.concat(`${entry}`.padStart(9, ' '));
+        entry = fizzBuzzAt(i, tests);
+        result = result.concat(`${entry}`.padStart(padding, ' '));
 
         // every tenth line print a new line 
-        if (i % 10 === 0) {
+        if (i % entriesPerLine === 0) {
             result = result.concat('\n');
         }
-        
+
     }
     return result;
 }
 
-function fizzBuzzAt(num) {
-    if (num % 5 === 0 && num % 8 === 0) {
-        return 'fizzbuzz';
+function fizzBuzzAt(num, tests) {
+    let result = "";
+    tests.forEach(t => {
+        if (num % t.value === 0) {
+            result = result.concat(t.result);
+        }
+    });
+    if (result === "") {
+        result = num;
     }
-    if (num % 5 === 0) {
-        return 'fizz';
-    }
-    if (num % 8 === 0) {
-        return 'buzz';
-    }
-    return num;
+    return result;
 }
